b2a4af247af2e468eef21bae3bb670f2c191ebfa
LokiTheMango <mirkomueller97@live.de>
2019-01-24 21:56:41 +0100

    Added db-connection php script

---
+++ php-scripts/db-connection.php
@@ -0,0 +1,21 @@
+<?php
+
+function OpenCon()
+ {
+ $dbhost = "localhost";
+ $dbuser = "root";
+ $dbpass = "1234";
+ $db = "test";
+
+
+ $conn = new mysqli($dbhost, $dbuser, $dbpass,$db) or die("Connect failed: %s\n". $conn -> error);
+ 
+ return $conn;
+ }
+ 
+function CloseCon($conn)
+ {
+ $conn -> close();
+ }
+   
+?>
