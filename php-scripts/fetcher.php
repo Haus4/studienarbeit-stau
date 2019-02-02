149f7a75a4bda2603aaa0bc850c1fae28e2ed661
LokiTheMango <mirkomueller97@live.de>
2019-02-02 17:25:27 +0100

    correct ordering in fetcher

--- php-scripts/fetcher.php
+++ php-scripts/fetcher.php
@@ -2,7 +2,7 @@
 include 'db-connection.php';
 header('Content-type:application/octet-stream');
 $conn = OpenCon();
-$query = "SELECT image FROM images ORDER BY inserttimestamp LIMIT 5";
+$query = "SELECT image FROM images ORDER BY inserttimestamp DESC LIMIT 5";
 if ($result = $conn->query($query)) {
     while($row = $result->fetch_assoc()) {
         $value = $row["image"];
