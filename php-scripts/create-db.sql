cec5d88251ab55813b8a420169d37247fb169cf4
LokiTheMango <mirkomueller97@live.de>
2019-02-21 16:38:58 +0100

    update schema: add masks table

--- php-scripts/create-db.sql
+++ php-scripts/create-db.sql
@@ -1,3 +1,4 @@
 CREATE DATABASE stauapp;
 CREATE TABLE cameras (ID char(5), description varchar(100), latitude int, longitude int);
-CREATE TABLE images (inserttimestamp DateTime NOT NULL UNIQUE, camera_id char(5), image blob, PRIMARY KEY (inserttimestamp));
+CREATE TABLE images (inserttimestamp DateTime NOT NULL UNIQUE, camera_id char(5), image mediumblob, PRIMARY KEY (inserttimestamp, camera_id));
+CREATE TABLE masks (camera_id char(5), orientation bit, image mediumblob, PRIMARY KEY(camera_id, orientation));
