ca303c66adfa077ad53cede27296376bf823b707
LokiTheMango <mirkomueller97@live.de>
2019-02-21 16:43:14 +0100

    cleanup fetchmask script

--- php-scripts/fetchmask.php
+++ php-scripts/fetchmask.php
@@ -4,11 +4,6 @@ header('Content-type:image/png');
 $conn = OpenCon();
 $cameraId = $_GET["camera"];
 $orientationId = $_GET["orientation"];
-$tscondition = "";
-if(array_key_exists("time", $_GET)){
-    $datetime = date("Y-m-d H:i:s", $_GET["time"]);
-    $tscondition = " AND inserttimestamp > '" . $datetime . "'";
-}
 $query = "SELECT image FROM masks WHERE camera_id=? AND orientation=?";
 $stmt = $conn->prepare($query);
 $stmt->bind_param("si", $cameraId, $orientationId);
