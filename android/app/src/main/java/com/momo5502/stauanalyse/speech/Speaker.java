ee10c6b93948c3ac03d46c88e8b69aac1207c10a
momo5502 <mauriceheumann@gmail.com>
2019-02-28 17:43:23 +0100

    Add SCO support

--- android/app/src/main/java/com/momo5502/stauanalyse/speech/Speaker.java
+++ android/app/src/main/java/com/momo5502/stauanalyse/speech/Speaker.java
@@ -1,22 +1,51 @@
 package com.momo5502.stauanalyse.speech;
 
 import android.content.Context;
+import android.media.AudioManager;
 import android.speech.tts.TextToSpeech;
 import android.speech.tts.TextToSpeech.OnInitListener;
+import android.speech.tts.UtteranceProgressListener;
 
+import java.util.HashSet;
 import java.util.LinkedList;
 import java.util.Locale;
 import java.util.Queue;
+import java.util.Set;
+
+import static android.content.Context.AUDIO_SERVICE;
 
 public class Speaker implements OnInitListener {
 
     private TextToSpeech textToSpeech;
+    private Context context;
     private boolean initialized = false;
+    private volatile int id = 1;
     private Queue<String> messageQueue = new LinkedList<>();
 
-
     public Speaker(Context context) {
+        this.context = context;
         textToSpeech = new TextToSpeech(context, this);
+        textToSpeech.setOnUtteranceProgressListener(new UtteranceProgressListener() {
+
+            @Override
+            public void onStart(String s) {
+
+            }
+
+            @Override
+            public void onDone(String s) {
+                if (Integer.parseInt(s) == id) {
+                    setBluetoothSco(false);
+                }
+            }
+
+            @Override
+            public void onError(String s) {
+                if (Integer.parseInt(s) == id) {
+                    setBluetoothSco(false);
+                }
+            }
+        });
     }
 
     public void speak(String text) {
@@ -30,7 +59,27 @@ public class Speaker implements OnInitListener {
     }
 
     private void speakInternal(String text) {
-        textToSpeech.speak(text, TextToSpeech.QUEUE_ADD, null);
+        int messageId = ++id;
+        setBluetoothSco(true);
+        textToSpeech.speak(text, TextToSpeech.QUEUE_ADD, null, messageId + "");
+    }
+
+    private void setBluetoothSco(boolean value) {
+        AudioManager audioManager = (AudioManager) context.getSystemService(AUDIO_SERVICE);
+
+        if (value && !audioManager.isBluetoothScoOn()) {
+            audioManager.setMode(AudioManager.MODE_IN_CALL);
+            audioManager.startBluetoothSco();
+            audioManager.setBluetoothScoOn(true);
+
+            awaitAdaptation();
+        } else if (!value && audioManager.isBluetoothScoOn()) {
+            awaitAdaptation();
+
+            audioManager.setMode(AudioManager.MODE_NORMAL);
+            audioManager.setBluetoothScoOn(false);
+            audioManager.stopBluetoothSco();
+        }
     }
 
     @Override
@@ -44,6 +93,14 @@ public class Speaker implements OnInitListener {
                     speakInternal(messageQueue.remove());
                 }
             }
+        }
+    }
+
+    private static void awaitAdaptation() {
+        try {
+            Thread.sleep(3000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
         }
     }
 }
